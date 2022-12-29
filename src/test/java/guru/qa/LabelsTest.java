6f4129e0d4636f90178d3191e853c652d74df158
Andrey Mozerov <andrey-mozerov@mail.ru>
2022-12-29 13:38:07 +0300

    v4

--- src/test/java/guru/qa/LabelsTest.java
+++ src/test/java/guru/qa/LabelsTest.java
@@ -22,7 +22,12 @@ public class LabelsTest {
 
     @Test
     public  void testDynamicLabels(){
-
+        Allure.getLifecycle().updateTestCase(t -> t.setName("Создание Issue для авторизованного пользователя"));
+        Allure.feature("Issue в репозитории");
+        Allure.story("Создание Issue");
+        Allure.label("owner", "eroshenkoam");
+        Allure.label("severity", SeverityLevel.CRITICAL.value());
+        Allure.link("Testing", "https://testing.github.com");
 
     }
 }
